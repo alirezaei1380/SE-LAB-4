e62a1befc9d99d010d6276275ea2214ccd51d002
Ali Rezaei <alirezaeim1380@gmail.com>
2023-11-18 14:56:53 +0330

    feat(transition): improve standard transition strategy tests

--- src/StandardTransitionStrategyTest.java
+++ src/StandardTransitionStrategyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StandardTransitionStrategyTest {
 
     @Test
-    @DisplayName("simple delivered state status test")
+    @DisplayName("simple standard transition price test")
     void standardTransitionPriceTest() {
         StandardTransitionStrategy strategy = new StandardTransitionStrategy();
         assertEquals(25, strategy.getPrice(10));
